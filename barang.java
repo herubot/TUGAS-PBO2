8ed498568671d43a134b3d196ffead8ef12578a5
herubot <113754059+herubot@users.noreply.github.com>
2022-11-24 20:02:22 +0700

    Add files via upload

---
+++ barang.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+     * Rivanda Putra Pratama Surahman
+     * 202043500136
+     * X5F
+     */
+    
+public abstract class barang {
+
+    public abstract void listBarang();
+   
+}
